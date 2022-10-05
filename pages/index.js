1b54e91b8dcd879afd2416cd9cdb6fa8115bbb12
Alex Kio <alexandermartelkio@gmail.com>
2022-10-05 18:08:29 -0400

    Check if user already logged in

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,34 @@
+import { useEffect } from "react";
 import Link from "next/link";
 import UserForm from "../components/userForm";
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
 import { useRouter } from "next/router";
 import { auth } from "./firebase";
 
 export default function Home() {
   const router = useRouter();
 
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        router.push({
+          pathname: "/store",
+        });
+      }
+    });
+  }, []);
+
   const loginUser = (email, password) => {
     signInWithEmailAndPassword(auth, email, password)
-      .then(cred => {
-        console.log('user logged in:', cred.user)
+      .then((cred) => {
+        console.log("user logged in:", cred.user);
         router.push({
           pathname: "/store",
         });
       })
-      .catch(err => {
-        console.log(err.message)
-      })
+      .catch((err) => {
+        console.log(err.message);
+      });
   };
 
   return (
