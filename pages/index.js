adda5d8520de1f2f69aece1f522f91f9b846ce84
Alex Kio <alexandermartelkio@gmail.com>
2022-10-05 16:59:56 -0400

    Create signup functionality

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,33 @@
-import { useEffect, useState } from 'react'
-import { db } from "./firebase"
-import { collection, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react";
+import Link from "next/link";
+import { db } from "./firebase";
+import { collection, getDocs } from "firebase/firestore";
+import UserForm from "../components/userForm";
 
 export default function Home() {
-  const [items, setItems] = useState([])
-  const itemsCollectionRef = collection(db, "items")
+  const [items, setItems] = useState([]);
+  const itemsCollectionRef = collection(db, "items");
 
   useEffect(() => {
     const getItems = async () => {
-      const data = await getDocs(itemsCollectionRef)
-      setItems(data.docs.map((doc) => ({id: doc.id, ...doc.data()})))
-      console.log(items)
-    }
+      const data = await getDocs(itemsCollectionRef);
+      setItems(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
+      console.log(items);
+    };
 
-    getItems()
-  }, [])
+    getItems();
+  }, []);
+
+  const loginUser = (email, password) => {
+    console.log(email, password);
+  };
 
   return (
-    <h1>hello</h1>
-  )
+    <>
+      <UserForm submissionHandler={loginUser} submitText={"Log In"} />
+      <Link href="/signup">
+        <a>Sign Up</a>
+      </Link>
+    </>
+  );
 }
