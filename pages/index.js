4eb0bf69405de2a84525c288d505c762c7d308df
Alex Kio <alexandermartelkio@gmail.com>
2022-10-05 17:26:18 -0400

    Move getItems to store page

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,11 @@
-import { useEffect, useState } from "react";
 import Link from "next/link";
-import { db } from "./firebase";
-import { collection, getDocs } from "firebase/firestore";
 import UserForm from "../components/userForm";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { useRouter } from "next/router";
 import { auth } from "./firebase";
 
 export default function Home() {
-  const [items, setItems] = useState([]);
-  const itemsCollectionRef = collection(db, "items");
   const router = useRouter();
-
-  useEffect(() => {
-    const getItems = async () => {
-      const data = await getDocs(itemsCollectionRef);
-      setItems(data.docs.map((doc) => ({ id: doc.id, ...doc.data() })));
-      console.log(items);
-    };
-
-    getItems();
-  }, []);
 
   const loginUser = (email, password) => {
     signInWithEmailAndPassword(auth, email, password)
