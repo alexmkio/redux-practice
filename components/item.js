ebe09bfb70c067966ef7b72dcfe97d7fc481a50f
Alex Kio <alexandermartelkio@gmail.com>
2022-10-10 17:36:40 -0400

    Display item qty in user cart

--- components/item.js
+++ components/item.js
@@ -1,9 +1,20 @@
+import { useSelector } from "react-redux";
+
 export default function Item({ item }) {
+  const userCart = useSelector((state) => state.user.cart);
+  let itemInCart = userCart.find((element) => element.item === item.id);
+  if (!itemInCart) {
+    itemInCart = {
+      qty: 0,
+    }
+  }
+
   return (
     <article>
       <h3>{item.brand}</h3>
       <p>{item.model}</p>
       <p>{item.price}</p>
+      <p>QTY: {itemInCart.qty}</p>
     </article>
   );
 }
